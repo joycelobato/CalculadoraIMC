4a1100ef60081bf043992d5b3456e4f55b836e1b
JOYCE LOBATO <JOYCELOBATOFRX@GMAIL.COM>
2024-04-14 19:53:48 +0100

    Calculando o IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -16,7 +16,13 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val altura = edtAltura.text
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+
+            println("joyce acao do botao" + resultado)
+
     }
 }}
