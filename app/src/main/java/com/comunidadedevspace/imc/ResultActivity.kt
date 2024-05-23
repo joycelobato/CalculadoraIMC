6785c83fd682f0fa9d6c8d08e41a04d53b785150
JOYCE LOBATO <JOYCELOBATOFRX@GMAIL.COM>
2024-05-23 17:15:38 +0100

    Merge remote-tracking branch 'origin/ATUALIZAÇÃO-BARRA-TRANSPARENTE' into ATUALIZAÇÃO-BARRA-TRANSPARENTE
    # Conflicts:
    #	app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 
 const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
